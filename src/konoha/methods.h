51c2d72aced9d274f9895738918d0acafa3e615b
masa <masa@c406673e-950c-47ff-9500-ef9fbfbf93bd>
2012-04-04 08:20:16 +0000

    fix method decl comments
    git-svn-id: svn+ssh://www.ubicg.ynu.ac.jp/svn/konoha-project/konoha2/trunk@302 c406673e-950c-47ff-9500-ef9fbfbf93bd

--- src/konoha/methods.h
+++ src/konoha/methods.h
@@ -8,78 +8,79 @@ static KMETHOD Object_opUPCAST(CTX, ksfp_t *sfp _RIX)
 	RETURN_(sfp[0].o);
 }
 
-/* !boolean */
+//## @Const method Boolean Boolean.opNOT();
 static KMETHOD Boolean_opNOT(CTX, ksfp_t *sfp _RIX)
 {
 	RETURNb_(!sfp[0].bvalue);
 }
 
-/* int + int */
+//## @Const method Int Int.opADD(Int x);
 static KMETHOD Int_opADD(CTX, ksfp_t *sfp _RIX)
 {
 	RETURNi_(sfp[0].ivalue + sfp[1].ivalue);
 }
 
-/* int - int */
+//## @Const method Int Int.opSUB(Int x);
 static KMETHOD Int_opSUB(CTX, ksfp_t *sfp _RIX)
 {
 	RETURNi_(sfp[0].ivalue - sfp[1].ivalue);
 }
 
-/* int * int */
+//## @Const method Int Int.opMUL(Int x);
 static KMETHOD Int_opMUL(CTX, ksfp_t *sfp _RIX)
 {
 	RETURNi_(sfp[0].ivalue * sfp[1].ivalue);
 }
 
-/* int / int */
+//## @Const method Int Int.opDIV(Int x);
 static KMETHOD Int_opDIV(CTX, ksfp_t *sfp _RIX)
 {
 	RETURNi_(sfp[0].ivalue / sfp[1].ivalue);
 }
 
-/* int % int */
+//## @Const method Int Int.opMOD(Int x);
 static KMETHOD Int_opMOD(CTX, ksfp_t *sfp _RIX)
 {
 	RETURNi_(sfp[0].ivalue % sfp[1].ivalue);
 }
 
-/* int == int */
+//## @Const method Boolean Int.opEQ(Int x);
 static KMETHOD Int_opEQ(CTX, ksfp_t *sfp _RIX)
 {
 	RETURNb_(sfp[0].ivalue == sfp[1].ivalue);
 }
 
-/* int != int */
+//## @Const method Boolean Int.opNEQ(Int x);
 static KMETHOD Int_opNEQ(CTX, ksfp_t *sfp _RIX)
 {
 	RETURNb_(sfp[0].ivalue != sfp[1].ivalue);
 }
 
-/* int < int */
+//## @Const method Boolean Int.opLT(Int x);
 static KMETHOD Int_opLT(CTX, ksfp_t *sfp _RIX)
 {
 	RETURNb_(sfp[0].ivalue < sfp[1].ivalue);
 }
 
-/* int <= int */
+//## @Const method Boolean Int.opLTE(Int x);
 static KMETHOD Int_opLTE(CTX, ksfp_t *sfp _RIX)
 {
 	RETURNb_(sfp[0].ivalue <= sfp[1].ivalue);
 }
 
-/* int > int */
+//## @Const method Boolean Int.opGT(Int x);
 static KMETHOD Int_opGT(CTX, ksfp_t *sfp _RIX)
 {
 	RETURNb_(sfp[0].ivalue > sfp[1].ivalue);
 }
 
-/* int >= int */
+//## @Const method Boolean Int.opGTE(Int x);
 static KMETHOD Int_opGTE(CTX, ksfp_t *sfp _RIX)
 {
 	RETURNb_(sfp[0].ivalue >= sfp[1].ivalue);
 }
 
+//## @Const method String Int.toString();
 /* Int to String */
 static KMETHOD Int_toString(CTX, ksfp_t *sfp _RIX)
 {
@@ -88,15 +89,13 @@ static KMETHOD Int_toString(CTX, ksfp_t *sfp _RIX)
 	RETURN_(new_kString(buf, strlen(buf), SPOL_ASCII));
 }
 
-/* String to Int */
+//## @Const method String Int.toString();
 static KMETHOD String_toInt(CTX, ksfp_t *sfp _RIX)
 {
 	RETURNi_((kint_t)strtoll(S_text(sfp[0].s), NULL, 10));
 }
 
-/* ------------------------------------------------------------------------ */
 //## @Const method String Boolean.toString();
-/* Booelan to String */
 static KMETHOD Boolean_toString(CTX, ksfp_t *sfp _RIX)
 {
 	kbool_t b = sfp[0].bvalue;
@@ -104,11 +103,11 @@ static KMETHOD Boolean_toString(CTX, ksfp_t *sfp _RIX)
 	RETURN_(new_kString(s[b], strlen(s[b]), SPOL_ASCII));
 }
 
-// void System.p(@Coercion String msg);
+//## method void System.p(@Coercion String msg);
 static KMETHOD System_p(CTX, ksfp_t *sfp _RIX)
 {
 	kline_t uline = sfp[K_RTNIDX].uline;
-	kreportf(PRINT_, uline, "%s\n", S_text(sfp[1].s));
+	kreportf(PRINT_, uline, "%s", S_text(sfp[1].s));
 }
 
 #ifdef __cplusplus
