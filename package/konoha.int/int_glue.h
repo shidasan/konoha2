f51d65af23c2f7dabbcbd15ea3c404e109739800
shinpei <shinpei@c406673e-950c-47ff-9500-ef9fbfbf93bd>
2012-05-02 12:06:07 +0000

    konoha2 konoha.int added (temporary)
    git-svn-id: svn+ssh://www.ubicg.ynu.ac.jp/svn/konoha-project/konoha2/trunk@472 c406673e-950c-47ff-9500-ef9fbfbf93bd

---
+++ package/konoha.int/int_glue.h
@@ -0,0 +1,89 @@
+/****************************************************************************
+ * Copyright (c) 2012, the Konoha project authors. All rights reserved.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ ***************************************************************************/
+
+/* ************************************************************************ */
+#ifndef INT_GLUE_H_
+#define INT_GLUE_H_
+
+#include <konoha2/konoha2.h>
+#include <konoha2/sugar.h>
+#include <konoha2/klib.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* ------------------------------------------------------------------------ */
+#define _Public   kMethod_Public
+#define _Const    kMethod_Const
+#define _Im       kMethod_Immutable
+#define _Coercion kMethod_Coercion
+#define _F(F)   (intptr_t)(F)
+
+static kbool_t int_initPackage(CTX, kKonohaSpace *ks, int argc, const char**args, kline_t pline)
+{
+	int FN_encoding = FN_("encoding");
+	intptr_t methoddata[] = {
+		DEND,
+	};
+	kKonohaSpace_loadMethodData(NULL, methoddata);
+	return true;
+}
+
+static kbool_t int_setupPackage(CTX, kKonohaSpace *ks, kline_t pline)
+{
+	return true;
+}
+
+#define TOKEN(T)  .name = T /*, .namelen = (sizeof(T)-1)*/
+#define _EXPR     .flag = SYN_ExprFlag
+#define ParseStmt_(NAME)  .ParseStmt = ParseStmt_##NAME
+#define ParseExpr_(NAME)   .ParseExpr = ParseExpr_##NAME
+#define TopStmtTyCheck_(NAME)  .TopStmtTyCheck = StmtTyCheck_##NAME
+#define StmtTyCheck_(NAME)     .StmtTyCheck = StmtTyCheck_##NAME
+#define ExprTyCheck_(NAME)     .ExprTyCheck = ExprTyCheck_##NAME
+#define _TERM     ParseExpr_(Term)
+#define _OP       ParseExpr_(Op)
+
+static kbool_t int_initKonohaSpace(CTX,  kKonohaSpace *ks, kline_t pline)
+{
+	USING_SUGAR;
+	KDEFINE_SYNTAX SYNTAX[] = {
+			{ TOKEN("<<"), _OP, .op2 = "opLShift", .priority_op2 = 1024, .right = 1, ExprTyCheck_(call), },
+			{ TOKEN(">>"), _OP, .op2 = "opRShift", .priority_op2 = 1024, .right = 1, ExprTyCheck_(call), },
+			{ .name = NULL, },
+	};
+	SUGAR KonohaSpace_defineSyntax(_ctx, ks, SYNTAX);
+	return true;
+}
+
+static kbool_t int_setupKonohaSpace(CTX, kKonohaSpace *ks, kline_t pline)
+{
+	return true;
+}
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INT_GLUE_H_ */
