75431eacc24ff2e1072b951f7cd21c5fe0dd6185
ishii <ishii@c406673e-950c-47ff-9500-ef9fbfbf93bd>
2012-05-29 07:58:29 +0000

    konoha2 posix.fd update
    git-svn-id: svn+ssh://www.ubicg.ynu.ac.jp/svn/konoha-project/konoha2/trunk@730 c406673e-950c-47ff-9500-ef9fbfbf93bd

--- package/posix.fd/fd_glue.h
+++ package/posix.fd/fd_glue.h
@@ -41,11 +41,11 @@ static KMETHOD System_lseek(CTX, ksfp_t *sfp _RIX)
 	off_t ret_offset = lseek(fd, offset, whence);
 	if (ret_offset == -1) {
 		ktrace(_DataFault,
-				KEYVALUE_s("@", "lseek"),
-				KEYVALUE_u("offset", offset),
-				KEYVALUE_u("whence", whence),
-				KEYVALUE_s("errstr", strerror(errno))
-		);
+			   KEYVALUE_s("@", "lseek"),
+			   KEYVALUE_u("offset", offset),
+			   KEYVALUE_u("whence", whence),
+			   KEYVALUE_s("errstr", strerror(errno))
+			);
 	}
 	RETURNi_((int)ret_offset);
 }
@@ -58,10 +58,10 @@ static KMETHOD System_truncate(CTX, ksfp_t *sfp _RIX)
 	int ret = ftruncate(fd, length);
 	if (ret != 0) {
 		ktrace(_SystemFault,
-				KEYVALUE_s("@", "ftruncate"),
-				KEYVALUE_u("length", length),
-				KEYVALUE_s("errstr", strerror(errno))
-		);
+			   KEYVALUE_s("@", "truncate"),
+			   KEYVALUE_u("length", length),
+			   KEYVALUE_s("errstr", strerror(errno))
+			);
 	}
 	RETURNb_(ret == 0);
 }
@@ -74,10 +74,10 @@ static KMETHOD System_chmod(CTX, ksfp_t *sfp _RIX)
 	int ret = fchmod(fd, mode);
 	if (ret != -1) {
 		ktrace(_SystemFault,
-				KEYVALUE_s("@", "fchmod"),
-				KEYVALUE_u("mode", mode),
-				KEYVALUE_s("errstr", strerror(errno))
-		);
+			   KEYVALUE_s("@", "chmod"),
+			   KEYVALUE_u("mode", mode),
+			   KEYVALUE_s("errstr", strerror(errno))
+			);
 	}
 	RETURNb_(ret == 0);
 }
@@ -92,11 +92,11 @@ static KMETHOD System_chown(CTX, ksfp_t *sfp _RIX)
 	int ret = fchown(fd, owner, group);
 	if (ret != -1) {
 		ktrace(_SystemFault,
-				KEYVALUE_s("@", "fchown"),
-				KEYVALUE_u("owner", owner),
-				KEYVALUE_u("group", group),
-				KEYVALUE_s("errstr", strerror(errno))
-		);
+			   KEYVALUE_s("@", "chown"),
+			   KEYVALUE_u("owner", owner),
+			   KEYVALUE_u("group", group),
+			   KEYVALUE_s("errstr", strerror(errno))
+			);
 	}
 	RETURNb_(ret == 0);
 }
@@ -129,10 +129,10 @@ static KMETHOD System_flock(CTX, ksfp_t *sfp _RIX)
 	int ret = flock(fd, operation);
 	if (ret == -1) {
 		ktrace(_SystemFault,
-				KEYVALUE_s("@", "flock"),
-				KEYVALUE_u("operation", operation),
-				KEYVALUE_s("errstr", strerror(errno))
-		);
+			   KEYVALUE_s("@", "flock"),
+			   KEYVALUE_u("operation", operation),
+			   KEYVALUE_s("errstr", strerror(errno))
+			);
 	}
 	RETURNb_(ret == 0);
 }
@@ -144,13 +144,182 @@ static KMETHOD System_sync(CTX, ksfp_t *sfp _RIX)
 	int ret =  fsync(fd);
 	if (ret == -1) {
 		ktrace(_SystemFault,
-				KEYVALUE_s("@", "fsync"),
-				KEYVALUE_p("errstr", strerror(errno))
-		);
+			   KEYVALUE_s("@", "fsync"),
+			   KEYVALUE_p("errstr", strerror(errno))
+			);
 	}
 	RETURNb_(ret == 0);
 }
 
+static KMETHOD System_link(CTX, ksfp_t *sfp _RIX)
+{
+	kString *s1 = sfp[1].s;
+	kString *s2 = sfp[2].s;
+	const char *oldpath = S_text(s1);
+	const char *newpath = S_text(s2);
+	int ret = link(oldpath, newpath);
+	if (ret == -1) {
+		ktrace(_SystemFault,
+			   KEYVALUE_s("@", "link"),
+			   KEYVALUE_s("oldpath", oldpath),
+			   KEYVALUE_s("newpath", newpath),
+			   KEYVALUE_p("errstr", strerror(errno))
+			);
+	}
+	RETURNi_(ret);
+}
+
+static KMETHOD System_unlink(CTX, ksfp_t *sfp _RIX)
+{
+	kString *s = sfp[1].s;
+	const char *pathname = S_text(s);
+	int ret = unlink(pathname);
+	if (ret == -1) {
+		ktrace(_SystemFault,
+			   KEYVALUE_s("@", "unlink"),
+			   KEYVALUE_s("pathname", pathname),
+			   KEYVALUE_p("errstr", strerror(errno))
+			);
+	}
+	RETURNi_(ret);
+}
+
+static KMETHOD System_rename(CTX, ksfp_t *sfp _RIX)
+{
+	kString *s1 = sfp[1].s;
+	kString *s2 = sfp[2].s;
+	const char *oldpath = S_text(s1);
+	const char *newpath = S_text(s2);
+	int ret = rename(oldpath, newpath);
+	if (ret == -1) {
+		ktrace(_SystemFault,
+			   KEYVALUE_s("@", "rename"),
+			   KEYVALUE_s("oldpath", oldpath),
+			   KEYVALUE_s("newpath", newpath),
+			   KEYVALUE_p("errstr", strerror(errno))
+			);
+	}
+	RETURNi_(ret);
+}
+
+static KMETHOD System_rmdir(CTX, ksfp_t *sfp _RIX)
+{
+	kString *s = sfp[1].s;
+	const char *pathname = S_text(s);
+	int ret = rmdir(pathname);
+	if (ret == -1) {
+		ktrace(_SystemFault,
+			   KEYVALUE_s("@", "rmdir"),
+			   KEYVALUE_s("pathname", pathname),
+			   KEYVALUE_p("errstr", strerror(errno))
+			);
+	}
+	RETURNi_(ret);
+}
+
+static KMETHOD System_symlink(CTX, ksfp_t *sfp _RIX)
+{
+	kString *s1 = sfp[1].s;
+	kString *s2 = sfp[2].s;
+	const char *oldpath = S_text(s1);
+	const char *newpath = S_text(s2);
+	int ret = symlink(oldpath, newpath);
+	if (ret == -1) {
+		ktrace(_SystemFault,
+			   KEYVALUE_s("@", "symlink"),
+			   KEYVALUE_s("oldpath", oldpath),
+			   KEYVALUE_s("newpath", newpath),
+			   KEYVALUE_p("errstr", strerror(errno))
+			);
+	}
+	RETURNi_(ret);
+}
+
+static KMETHOD System_readlink(CTX, ksfp_t *sfp _RIX)
+{
+	kString *s1 = sfp[1].s;
+	kString *s2 = sfp[2].s;
+	const char *pathname = S_text(s1);
+	const char *buf = S_text(s2);
+	size_t bufsize = strlen(buf);
+	ssize_t ret = readlink(pathname, (char *)buf, bufsize);
+	if (ret == -1) {
+		ktrace(_SystemFault,
+			   KEYVALUE_s("@", "readlink"),
+			   KEYVALUE_s("pathname", pathname),
+			   KEYVALUE_s("buf", buf),
+			   KEYVALUE_u("bufsize", bufsize),
+			   KEYVALUE_p("errstr", strerror(errno))
+			);
+	}
+	RETURNi_(ret);
+}
+
+static KMETHOD System_lchown(CTX, ksfp_t *sfp _RIX)
+{
+	kString *s = sfp[1].s;
+	const char *pathname = S_text(s);
+	uid_t owner = sfp[2].ivalue;
+	gid_t group = sfp[3].ivalue;
+	int ret = lchown(pathname, owner, group);
+	if (ret == -1) {
+		ktrace(_SystemFault,
+			   KEYVALUE_s("@", "lchown"),
+			   KEYVALUE_s("pathname", pathname),
+			   KEYVALUE_u("owner", owner),
+			   KEYVALUE_u("group", group),
+			   KEYVALUE_p("errstr", strerror(errno))
+			);
+	}
+	RETURNi_(ret);
+}
+
+static KMETHOD System_fchown(CTX, ksfp_t *sfp _RIX)
+{
+	int fd = sfp[1].ivalue;
+	uid_t owner = sfp[2].ivalue;
+	gid_t group = sfp[3].ivalue;
+	int ret = fchown(fd, owner, group);
+	if (ret == -1) {
+		ktrace(_SystemFault,
+			   KEYVALUE_s("@", "fchown"),
+			   KEYVALUE_u("owner", owner),
+			   KEYVALUE_u("group", group),
+			   KEYVALUE_p("errstr", strerror(errno))
+			);
+	}
+	RETURNi_(ret);
+}
+
+static KMETHOD System_access(CTX, ksfp_t *sfp _RIX)
+{
+	kString *s = sfp[1].s;
+	const char *pathname = S_text(s);
+	int mode = sfp[2].ivalue;
+	int ret = access(pathname, mode);
+	if (ret == -1) {
+		ktrace(_SystemFault,
+			   KEYVALUE_s("@", "access"),
+			   KEYVALUE_s("pathname", pathname),
+			   KEYVALUE_u("mode", mode),
+			   KEYVALUE_p("errstr", strerror(errno))
+			);
+	}
+	RETURNi_(ret);
+}
+
+static KMETHOD System_fsync(CTX, ksfp_t *sfp _RIX)
+{
+	int fd = sfp[1].ivalue;
+	int ret = fsync(fd);
+	if (ret == -1) {
+		ktrace(_SystemFault,
+			   KEYVALUE_s("@", "fsync"),
+			   KEYVALUE_p("errstr", strerror(errno))
+			);
+	}
+	RETURNi_(ret);
+}
 // --------------------------------------------------------------------------
 
 #define _Public   kMethod_Public
@@ -168,6 +337,16 @@ static kbool_t fd_initPackage(CTX, kKonohaSpace *ks, int argc, const char**args,
 		_Public|_Const|_Im, _F(System_chown), TY_Boolean, TY_System, MN_("chown"), 3, TY_Int, FN_("fd"), TY_Int, FN_("owner"), TY_Int, FN_("group"),
 		_Public|_Const|_Im, _F(System_flock), TY_Boolean, TY_System, MN_("flock"), 2, TY_Int, FN_("fd"), TY_Int, FN_("operation"),
 		_Public|_Const|_Im, _F(System_sync), TY_Boolean, TY_System, MN_("sync"), 1, TY_Int, FN_("fd"),
+		_Public|_Const|_Im, _F(System_link), TY_Int, TY_System, MN_("link"), 2, TY_String, FN_("oldpath"), TY_String, FN_("newpath"),
+		_Public|_Const|_Im, _F(System_unlink), TY_Int, TY_System, MN_("unlink"), 1, TY_String, FN_("pathname"),
+		_Public|_Const|_Im, _F(System_rename), TY_Int, TY_System, MN_("rename"), 2, TY_String, FN_("oldpath"), TY_String, FN_("newpath"),
+		_Public|_Const|_Im, _F(System_rmdir), TY_Int, TY_System, MN_("rmdir"), 1, TY_String, FN_("pathname"),
+		_Public|_Const|_Im, _F(System_symlink), TY_Int, TY_System, MN_("symlink"), 2, TY_String, FN_("oldpath"), TY_String, FN_("newpath"),
+		_Public|_Const|_Im, _F(System_readlink), TY_Int, TY_System, MN_("readlink"), 3, TY_String, FN_("pathname"), TY_String, FN_("buf"), TY_Int, FN_("bufsize"),
+		_Public|_Const|_Im, _F(System_lchown), TY_Int, TY_System, MN_("lchown"), 3, TY_String, FN_("pathname"), TY_Int, FN_("owner"), TY_Int, FN_("group"),
+		_Public|_Const|_Im, _F(System_fchown), TY_Int, TY_System, MN_("fchown"), 3, TY_Int, FN_("pd"), TY_Int, FN_("owner"), TY_Int, FN_("group"),
+		_Public|_Const|_Im, _F(System_access), TY_Int, TY_System, MN_("access"), 2, TY_String, FN_("pathname"), TY_Int, FN_("mode"),
+		_Public|_Const|_Im, _F(System_fsync), TY_Int, TY_System, MN_("fsync"), 1, TY_Int, FN_("fd"),
 		DEND,
 	};
 	kKonohaSpace_loadMethodData(ks, MethodData);
